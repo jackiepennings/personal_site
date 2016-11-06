b177dffb2c6c4e6e93b0977b457df2059cdbaba9
Jacquelyn Pennings <Jacquelyn Pennings>
2016-11-06 15:09:25 -0600

    try again

--- build_site.R
+++ build_site.R
@@ -2,8 +2,11 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("~/Desktop/Personal/personal_site")
+setwd("C:/Users/jacsp/Desktop/personal_site")
 
 #render your sweet site. 
 library("rmarkdown")
 rmarkdown::render_site()
+
+getwd()
+
