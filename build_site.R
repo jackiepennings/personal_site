9eb59e47a6aad424c92c731a3bf811cab2da89ee
Jacquelyn Pennings <Jacquelyn Pennings>
2017-03-18 23:22:22 -0500

    new job updates

--- build_site.R
+++ build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("C:/Users/JackiePennings/Desktop/Personal/personal_site")
+setwd("C:/Users/jacsp/Desktop/personal_site")
 
 #render your sweet site. 
 library("rmarkdown")
